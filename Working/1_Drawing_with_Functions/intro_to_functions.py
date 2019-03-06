b3c70a4d4598d27a219e1fcb6c3834c3587f3fcd
robuntu <robuntu@stro.ycdsb.ca>
2019-03-06 10:04:21 -0500

    Functions

--- Working/1_Drawing_with_Functions/intro_to_functions.py
+++ Working/1_Drawing_with_Functions/intro_to_functions.py
@@ -11,14 +11,15 @@ def main():
     arcade.set_background_color(arcade.color.DARK_BLUE)
     arcade.start_render()
 
-    arcade.draw_circle_filled(300, 330, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(250, 290, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(325, 290, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(300, 530, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(250, 490, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(325, 490, 50, arcade.color.WHITE)
 
-    arcade.draw_circle_filled(350, 330, 55, arcade.color.GREEN)
-    arcade.draw_circle_filled(300, 290, 55, arcade.color.GREEN)
-    arcade.draw_circle_filled(375, 290, 55, arcade.color.GREEN)
-    arcade.draw_rectangle_filled(350, 360, 50, )
+    arcade.draw_circle_filled(550, 330, 55, arcade.color.GREEN)
+    arcade.draw_circle_filled(500, 290, 55, arcade.color.GREEN)
+    arcade.draw_circle_filled(575, 290, 55, arcade.color.GREEN)
+    arcade.draw_rectangle_filled(550, 178, 60, 150,
+                              arcade.color.BRITISH_RACING_GREEN, 1)
 
    # call your draw functions
 
