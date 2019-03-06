5fbf401c38b8cd882de3d56e2fa27a08883a20a2
robuntu <robuntu@stro.ycdsb.ca>
2019-03-06 11:09:53 -0500

    Treefunction

--- Working/1_Drawing_with_Functions/intro_to_functions.py
+++ Working/1_Drawing_with_Functions/intro_to_functions.py
@@ -6,20 +6,31 @@ SCREEN_HEIGHT = 600
 # define your draw functions
 
 
+
+
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
-    arcade.set_background_color(arcade.color.DARK_BLUE)
+    arcade.set_background_color(arcade.color.BLUE)
     arcade.start_render()
 
-    arcade.draw_circle_filled(300, 530, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(250, 490, 50, arcade.color.WHITE)
-    arcade.draw_circle_filled(325, 490, 50, arcade.color.WHITE)
+def cloud(x,y,z):
+    arcade.draw_circle_filled(x - 200, 530, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(x - 150, 490, 50, arcade.color.WHITE)
+    arcade.draw_circle_filled(x -225, 490, 50, arcade.color.WHITE)
 
-    arcade.draw_circle_filled(550, 330, 55, arcade.color.GREEN)
-    arcade.draw_circle_filled(500, 290, 55, arcade.color.GREEN)
-    arcade.draw_circle_filled(575, 290, 55, arcade.color.GREEN)
-    arcade.draw_rectangle_filled(550, 178, 60, 150,
-                              arcade.color.BRITISH_RACING_GREEN, 1)
+def treebase(a,b,c,d):
+    arcade.draw_rectangle_filled(a - 100,b - 100, c - 100, , 150,
+                                 arcade.color.BRITISH_RACING_GREEN, 0)
+
+def treeleaves(e,f,g):
+    arcade.draw_circle_filled(555, 330, 60, arcade.color.GREEN_YELLOW)
+    arcade.draw_circle_filled(505, 290, 60, arcade.color.GREEN_YELLOW)
+    arcade.draw_circle_filled(580, 290, 60, arcade.color.GREEN_YELLOW)
+
+    arcade.draw_circle_filled(120, 2, 200, arcade.color.BOTTLE_GREEN)
+    arcade.draw_circle_filled(800, 2, 200, arcade.color.BOTTLE_GREEN)
+    arcade.draw_circle_filled(450, 2, 200, arcade.color.BOTTLE_GREEN)
+
 
    # call your draw functions
 
@@ -30,3 +41,11 @@ def main():
 
 # Call the main function to get the program started.
 main()
+
+cloud(300, 530, 50)
+cloud(250, 490, 50)
+could(325, 490, 50)
+treebase(550, 178, 60, 150)
+treeleaves(555, 330, 60)
+treeleaves(505, 290, 60)
+treeleaves(580, 290, 60)
